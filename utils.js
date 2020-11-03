7f0eaa8e2c601d318f542af410273dfe5d58f9cd
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2020-11-03 11:18:44 -0600

    bcrypt implementing

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 
 const csurf = require('csurf');
 const asyncHandler = (handler)=>(req, res, next)=>handler(req, res, next).catch(next)
-
+const bcrypt = require('bcryptjs')
 const csrfProtection = csurf({cookie:true});
 
 
