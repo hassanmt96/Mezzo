946b83e82b233e40e86c8bff8c0eab5f46082b52
salina kuo <salina.kuo97@gmail.com>
2020-11-04 17:50:33 -0500

    editing nav-bar

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { check } = require("express-validator");
 const { User } = require("../db/models");
-const { asyncHandler, csrfProtection, handleValidationErrors } = require("./utils");
+const { asyncHandler, csrfProtection, handleValidationErrors } = require("../utils");
 const { loginUser, logoutUser } = require("../auth");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
