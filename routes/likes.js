c0bbc26dbf4f3909dc76504384b69b9371b620ed
Safiya Cain <say.cain3@gmail.com>
2020-11-05 12:44:19 -0500

    create like ajax request

--- routes/likes.js
+++ routes/likes.js
@@ -1,4 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const likeButton = document.getElementById('likeBtn');
+likeButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const storyId = e.story.id;
+const res = await fetch('/read-a-story', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON. stringify(like)
+})
+const tweet = await res.json();
+likeButton.innerHTML = 'Liked'
+})
+
+router.post('like', async(req, res)=> {
+    res.render('readStory');
+})
+
 module.exports = router
