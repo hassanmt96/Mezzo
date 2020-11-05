5e22a785078317813fcdd642d8cd0c0ed313c1a7
salina kuo <salina.kuo97@gmail.com>
2020-11-05 11:12:36 -0500

    working on comments and follows

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const router = express.Router();
 const { requireAuth } = require('../auth');
 const { asyncHandler, handleValidationErrors } = require('../utils');
-const { Story, User } = require('../db/models');
+const { Story, Follow, User } = require('../db/models');
 
 
 /* GET home page/feed */
 router.get('/', asyncHandler(async(req, res, next) => {
   const stories = await Story.findAll({ order: [['title', 'ASC']]});
-  res.render('index', { title: 'Stories', stories });
+  // const followerId = /* insert session user.id */
+  // const following = Follow.findAll({ where: followerId });
+  // const authorId = following.followingId;
+  // const followingStories = Story.findAll({ where: authorId });
+  res.render('index', { stories, followingStories });
 }));
 
 
