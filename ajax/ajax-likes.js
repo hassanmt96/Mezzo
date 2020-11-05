ee4781cd1821c7a5fb423c9df3ed348496c41635
Safiya Cain <say.cain3@gmail.com>
2020-11-05 13:16:33 -0500

    create like route

---
+++ ajax/ajax-likes.js
@@ -0,0 +1,16 @@
+window.addEventListener("DOMContentLoaded", (e) => {
+    const likeButton = document.getElementById('likeBtn');
+likeButton.addEventListener('click', async(e) => {
+    e.preventDefault();
+    const storyId = e.story.id;
+const res = await fetch('/read-a-story', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON. stringify(like)
+})
+const story = await res.json();
+likeButton.innerHTML = 'Liked'
+})
+})
--- routes/likes.js
+++ routes/likes.js
@@ -1,21 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const likeButton = document.getElementById('likeBtn');
-likeButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    const storyId = e.story.id;
-const res = await fetch('/read-a-story', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON. stringify(like)
-})
-const tweet = await res.json();
-likeButton.innerHTML = 'Liked'
-})
-
 router.post('like', async(req, res)=> {
     res.render('readStory');
 })
