db4a92beac74fdd4b62615799b3fc9a4e4774670
Safiya Cain <say.cain3@gmail.com>
2020-11-05 18:17:39 -0500

    debugging innerHTML

--- public/javascripts/ajax-likes.js
+++ public/javascripts/ajax-likes.js
@@ -1,18 +1,22 @@
 window.addEventListener("DOMContentLoaded", (e) => {
-    console.log('test');
+    // console.log('test');
     const likeButton = document.getElementById('likeBtn');
-likeButton.addEventListener('click', async(e) => {
+    likeButton.addEventListener('click', async(e) => {
     e.preventDefault();
-    const storyId = e.story.id;
-const res = await fetch(`/stories/${storyId}/like`, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(like)
-})
-const story = await res.json();
-likeButton.innerHTML = 'Liked'
-})
+    const storyId = document.getElementById('story-Id').value;
+        const res = await fetch(`/stories/${storyId}/like`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify()
+        })
+    const isLiked = await res.json();
+    if(isLiked){
+        likeButton.innerHTML = 'Liked'
+    } else{
+        likeButton.innerHTML = 'Like'
+    }
+    })
 
 })
