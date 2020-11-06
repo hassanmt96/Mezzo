2b8a2aab894c9a9290933fdaf61d8746008c18b1
Jacob Grooms <grooms.jacob@gmail.com>
2020-11-05 21:12:05 -0500

    IT WORKS

--- public/javascripts/ajax-comments.js
+++ public/javascripts/ajax-comments.js
@@ -3,23 +3,23 @@ window.addEventListener('DOMContentLoaded', (e) => {
     commentBtn.addEventListener('click', async(e) => {
         e.preventDefault();
         const storyId = document.getElementById('story-Id').value;
-        const body = document.getElementById('commentBody').value
+        const comment = document.getElementById('commentBody').value
         const res = await fetch(`/stories/${storyId}/comment`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({body})
-        });
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+						"Accept": "application/json",
+					},
+					body: JSON.stringify({comment: comment})
+				});
         const jsonRes = await res.json();
         console.log(jsonRes);
-        const comments = document.getElementById('comments');
-        let commentsHtml = [];
-        Array.from(res).forEach((comment) => {
-            let html = `<p>${comment.comment}</p>`
-            commentsHtml.push(html);
-        });
-        comments.innerHTML = commentsHtml.join('');
+        // const comments = document.getElementById('comments');
+        // let commentsHtml = [];
+        // Array.from(res).forEach((comment) => {
+        //     let html = `<p>${comment.comment}</p>`
+        //     commentsHtml.push(html);
+        // });
+        // comments.innerHTML = commentsHtml.join('');
     })
 })
--- routes/stories.js
+++ routes/stories.js
@@ -36,17 +36,19 @@ router.get('/:id(\\d+)', asyncHandler(async(req, res, next) => {
 }));
 
 router.post('/:id(\\d+)/comment', asyncHandler(async(req, res) => {
-  const storyId = parseInt(req.params.id);
-  console.log(req.body);
-  const story = await Story.findByPk(storyId, {
-    include: User
-  });
+  // const storyId = parseInt(req.params.id);
+  // console.log(req.body);
+  // const story = await Story.findByPk(storyId, {
+  //   include: User
+  // });
+  // console.log('test')
+  const {comment} = req.body
+  await res.json(req.body)
   await Comment.create({
     userId: res.locals.user.id,
     storyId: req.params.id,
-    comment: req.body
+    comment,
   })
-
   const comments = await Comment.findAll({
     where: { storyId: req.params.id }
   });
