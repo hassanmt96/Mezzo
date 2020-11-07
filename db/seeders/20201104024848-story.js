89efcd4306fcceaab71db2c0e2ebf4beee841420
salina kuo <salina.kuo97@gmail.com>
2020-11-06 23:57:43 -0500

    seed

--- db/seeders/20201104024848-story.js
+++ db/seeders/20201104024848-story.js
@@ -6,10 +6,10 @@ module.exports = {
 			"Stories",
 			[
 				{
-					title: "A Story About Music",
-					subtitle: "This writing is, indeed, about music, somehow.",
+					title: "Composing For Household Objects",
+					subtitle: "Combatting classicism and erasure in western classical music",
 					content:
-						"words words words words words words words words words words words words words words words words words words words words words words words words",
+						"As a classical percussionist I’ve noticed how accessibility is perceived as novel or juvenile in academia. I’ve decided to start composing works for solo percussion using everyday objects (instead of the large, expensive gear students can use while they’re in university. After students graduate they often don’t have access to any instruments. This is my way of addressing classicism in classical percussion.) I set the music to text by writers of marginalized voices (to combat the erasure of nonwhite+nonman narratives in classical music). \n \n Salina Kuo is a Philadelphia-based person (settler of Lenape territory) interested in accessibility for music performance + music education. She opposes accessibility's \"novel\" reputation, and seeks to create nuanced, meaningful art from simplicity. She composes theatrical works for found objects, and releases pop music as St. John's Wort. \n \n She holds a B.M. in music education from Temple University. There she studied orchestral reportoire, jazz vibraphone, latin percussion, and drumset. She teaches elementary + middle school general music, private drumset + piano lessons, and volunteers with Girls Rock Philly. She recently performed at the Oh My Ears New Music Festival, Toronto Creative Music Lab, and Steve Weiss Mallet Festival.",
 					authorId: 1,
 					image:
 						"https://cdn11.bigcommerce.com/s-8wy6p2/images/stencil/1500x1000/uploaded_images/article.png?t=1575561837",
