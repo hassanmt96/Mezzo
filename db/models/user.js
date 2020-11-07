72f4afd0337e024d159b8a715bbb53e1c118d196
salina kuo <salina.kuo97@gmail.com>
2020-11-07 18:19:50 -0500

    user constraints

--- db/models/user.js
+++ db/models/user.js
@@ -6,12 +6,10 @@ module.exports = (sequelize, DataTypes) => {
 			firstName: {
 				type: DataTypes.STRING(50),
 				allowNull: false,
-				unique: true,
 			},
 			lastName: {
 				type: DataTypes.STRING(50),
 				allowNull: false,
-				unique: true,
 			},
 			email: {
 				type: DataTypes.STRING(100),
